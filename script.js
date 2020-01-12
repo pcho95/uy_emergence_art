1d2f557be2217a9cf557e9b510fc0dee2432f826
Paul Cho <pcho@colgate.edu>
2020-01-11 18:39:21 -0600

    updated for more control

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ console.log( canvas.style.cssText )
 
 
 const settings = {
-  thickness: 10 * (csize/3000),
   color: 'gray',
   color2: 'red'
 }
@@ -17,9 +16,14 @@ const settings = {
 
 let second_line_layer = 0;
 let second_line_freq = 0.75;
-let hole_fill = .8
+let first_hole_fill = .8;
+let second_hole_fill = .8;
 let second_line_min = 0.8;
 let second_line_max = 0.8;
+let bg = 0;
+
+let first_line_width = 10 * (csize/3000);
+let second_line_width = 10 * (csize/3000);
 
 /* * * * */
 /* ease of use */
@@ -43,14 +47,19 @@ function clamp(val,min,max) {
   return Math.min(max, Math.max(min, val));
 }
 
+function floor(x) {
+  return Math.floor(x);
+}
+
 /* * * * */
 
 /* Make sure background is cleared */
 surface.fillStyle = 'white';
 surface.fillRect(0, 0, csize, csize);
 
+
 /* Func: Draw a circle with X segments */
-function DrawCircle(x, segments, radius, fuzz, vscale, color) {
+function DrawCircle(x, segments, radius, fuzz, vscale, color, thickness) {
 
   /* Generate the pure points */
   var points = [];
@@ -88,7 +97,7 @@ function DrawCircle(x, segments, radius, fuzz, vscale, color) {
     let point_a = points[ ( (  index) % points.length) ];
     let point_b = points[ ( (index+1) % points.length) ];
 
-    surface.lineWidth   = settings.thickness;
+    surface.lineWidth   = thickness;
     surface.strokeStyle = color;
     surface.beginPath();
     surface.moveTo(point_a[0], point_a[1]);
@@ -98,28 +107,39 @@ function DrawCircle(x, segments, radius, fuzz, vscale, color) {
     /* Smooth end */
     surface.fillStyle = color;
     surface.beginPath();
-    surface.arc(point_b[0], point_b[1], settings.thickness*.5, 0, 2 * Math.PI);
+    surface.arc(point_b[0], point_b[1], thickness*.5, 0, 2 * Math.PI);
     surface.fill();
   }
 }
 
+
 let SKIP_SECOND=false;
-function Program(count,reverse=false){
 
-  /* get vals from DOM */
+function Program(_count,reverse=false){
+
+  let count_main = document.getElementById('first_line_freq').value * _count;
+
+  // Get vals from DOM
   second_line_freq = document.getElementById('second_line_freq').value;
-  hole_fill = 1.0 - document.getElementById('hole_fill').value;
-  /* * */
+  first_hole_fill = 1.0 - document.getElementById('first_hole_fill').value;
+  second_hole_fill = 1.0 - document.getElementById('second_hole_fill').value;
+  first_line_width = document.getElementById('first_line_width').value *2 * (3000/csize);
+  second_line_width = document.getElementById('second_line_width').value *2* (3000/csize);
 
-
+  // Render the first pass
   let mid_raw = 0.15;
-  let deadzones
-  if(!reverse) {
-    for(var index=0; index<count; index++) {
+  let first_line_min = document.getElementById('first_line_min').value;
+  let first_line_max = document.getElementById('first_line_max').value;
+  if (!reverse) {
+    for(var index=0; index<count_main; index++) {
 
-      let scale = index/count;
+      let scale = index/count_main;
 
-      if (scale < 0.78 && random() < (0.9 + (hole_fill*0.1))) {
+      if (scale < first_line_min || scale > first_line_max) {
+        continue;
+      }
+
+      if (scale < 0.78 && random() < (0.9 + (first_hole_fill*0.1))) {
         continue;
       }
 
@@ -127,38 +147,38 @@ function Program(count,reverse=false){
 
       let x = (csize*mid_raw) + power( 1.9, ramp);
 
-      let vscale = 1;
       let lim = 0.9;
-      if (scale > lim) {
-        vscale = 1 + ( (scale-lim)*10 )
-      }
+      let vscale = (scale > lim)
+        ? 1 + ( (scale-lim)*10 )
+        : 1;
 
-      //let radius = 90 + power(scale,1.9+ramp)*0.1 + (random()*6)
-      //if (ramp < 18)
       let segments = scale < 0.78
         ? 20
-        : 40 + Math.floor(40*scale);
+        : 40 + floor(40*scale);
 
       let fuzz = scale < 0.78
         ? 5
         : clamp(5 + (scale*4), 0, index*0.1)
 
       let pickiness = Math.max(.05, 0.8 - scale);
-      if(random()*random() > pickiness)
-      DrawCircle(
-        x,
-        segments,
-        5 + power(1.915,ramp*1.01) ,
-        fuzz,
-        vscale,
-        settings.color
-      );
+      if(random()*random() > pickiness) {
+        DrawCircle(
+          x,
+          segments,
+          5 + power(1.915,ramp*1.01) ,
+          fuzz,
+          vscale,
+          settings.color,
+          first_line_width
+        );
+      }
     }
   }
 
+  // Render the second pass
   let second_line_min = document.getElementById('second_line_min').value;
   let second_line_max = document.getElementById('second_line_max').value;
-  let sparse_count = Math.floor(count * (0.02 + (.08 * second_line_freq) ) );
+  let sparse_count = floor(_count * (0.02 + (.08 * second_line_freq) ) );
   if (!SKIP_SECOND) {
     for(var index=0; index<sparse_count; index++) {
 
@@ -168,37 +188,47 @@ function Program(count,reverse=false){
         continue;
       }
 
+      if (scale < 0.78 && random() < (0.9 + (second_hole_fill*0.1))) {
+        continue;
+      }
+
       let ramp = power(10, 1.1*scale)
 
       let x = (csize*mid_raw) + power( 1.9, ramp);
 
-      let vscale = 1;
       let lim = 0.9;
-      if (scale > lim) {
-        vscale = 1 + ( (scale-lim)*10 )
-      }
+      let vscale = (scale > lim)
+        ? 1 + ( (scale-lim)*10 )
+        : 1;
 
-      //let radius = 90 + power(scale,1.9+ramp)*0.1 + (random()*6)
-      //if (ramp < 18)
       let pickiness = Math.max(.05, 0.8 - scale);
-      if(random()*random() > pickiness)
-      DrawCircle(x, 40 + Math.floor(40*scale), 5 + power(1.915,ramp*1.01) , 2 + (scale*4), vscale, settings.color2 );
+      if (random()*random() > pickiness) {
+        DrawCircle(x,
+          40 + floor(40*scale),
+          5 + power(1.915,ramp*1.01),
+          2 + (scale*4),
+          vscale,
+          settings.color2,
+          second_line_width
+        );
+      }
     }
   }
+
   SKIP_SECOND=false;
-  if(reverse){
+
+  if (reverse) {
     SKIP_SECOND=true;
-    Program(count, false);
+    Program(_count, false);
   }
 }
 
-Program(1300);
-
+/* attach render call to run button */
 let run_button = document.getElementById('run');
 run_button.onclick = ()=>{
-  surface.fillStyle = 'white';
+  surface.fillStyle = bg ? 'black' : 'white';
   surface.fillRect(0, 0, csize, csize);
   Program(1300, second_line_layer);
 }
 
-//DrawCircle(500, 180, 800, 25);
+Program(1300);
